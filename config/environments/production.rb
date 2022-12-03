ac31cc2152b285309bcb5bae02c1e3a32387a812
田中宙 <tanakachuu@tanakachuunoAir.elecom>
2022-12-03 22:14:48 +0900

    gmail fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: '35.79.193.61', port: 80 }
+  config.action_mailer.default_url_options = {  :host => 'http://35.79.193.61', port: 80 }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {

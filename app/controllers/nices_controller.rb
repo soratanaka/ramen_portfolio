cce9e78922e0d681c6ae2bd4666be84d57c4fbda
田中宙 <tanakachuu@tanakachuunoAir.elecom>
2022-12-06 21:55:58 +0900

    post nice fix

--- app/controllers/nices_controller.rb
+++ app/controllers/nices_controller.rb
@@ -1,12 +1,11 @@
 class NicesController < ApplicationController
+  def create 
+    nice = current_user.nices.create(post_id: params[:post_id])
+    redirect_to post_path(nice.post.id), notice: "#{nice.post.user.name}さんの投稿にいいねしました"
+  end
 
-def create 
-  nice = current_user.nices.create(post_id: params[:post_id])
-  redirect_to posts_path, notice: "#{nice.post.user.name}を気になる登録しました"
-end
-
-def destroy
-  nice = current_user.nices.find_by(id: params[:id]).destroy
-  redirect_to posts_path, notice: "#{nice.post.user.name}を気になる登録解除しました"
-end
+  def destroy
+    nice = current_user.nices.find_by(id: params[:id]).destroy
+    redirect_to post_path(nice.post.id), notice: "#{nice.post.user.name}をんの投稿にいいねを解除しました"
+  end
 end
